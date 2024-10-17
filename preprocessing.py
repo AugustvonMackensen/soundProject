ac94b9e9caf228dac976c87eb5a3aa4fd9e8b04f
Leo Lee <bonaparte_19th@hotmail.com>
2024-10-17 14:54:08 +0900

    전처리 멜스토그램 실습

--- preprocessing.py
+++ preprocessing.py
@@ -1,20 +1,42 @@
 '''
 preprocessing.py : 전처리와 관련된 파이썬 파일입니다.
 '''
+import numpy as np
 from pyannote.audio import Model, Inference
+import matplotlib.pyplot as plt
 import librosa
+from librosa.feature import melspectrogram
 import torch
 
 '''
 test_librosa : librosa 전처리 코드
 '''
-def test_librosa():
+def test_librosa(file_path):
     # waveform : 실수형 ndarray -> 시각화하는데 사용
     # sr : sampling_rate
     waveform, sr = librosa.load('dataset/raw_data/2_1373G2A4_1447G2A5_T1_2D07T0333C000478_005449.wav')
 
     # 데이터 길이 / sampling rate -> 음성 파일 시간
     print(f'음성 시간 : {len(waveform) // sr // 3600}h: {len(waveform) // sr // 60}m: {len(waveform) // sr % 60}s')
+
+    # 멜스토그램 만들기
+    # win_length : 자를때 자를 작은 조각의 크기(기본이 25ms)
+    # n_fft : win_length 크기로 잘린 음성의 작은 조각은 0으로 패딩되어서 n_fft로 크기가 맞춰짐. (일반적으로, 속도를 위해서 2^n으로 설정해야 함, win_length보다 크거나 같아야 함)
+    # hop_length : 자르는 간격
+    # librosa.feature.melspectogram : melspectogram ->
+
+    win_length = int(np.ceil(0.025 * len(waveform)))
+    hop_length = int(np.ceil(0.001 * len(waveform)))
+
+
+
+
+def melstogram(waveform, sr, win_length, hop_length):
+    s = melspectrogram(y=waveform, sr=sr, n_fft=1024, win_length=win_length, hop_length=hop_length)
+    fig, ax = plt.subplots()
+    s_db = librosa.power_to_db(s, ref=np.max)
+    img = librosa.display.specshow(s_db, x_axis='time', y_axis='mel', sr=sr, ax=ax)
+
 
 
 
